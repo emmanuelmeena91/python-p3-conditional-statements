1643c1adcb8ceaa39e244b6ed5469b30bab2dc49
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-08-14 18:50:55 +0300

    complete conditions

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,80 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    print("Access granted") if ((username == "admin" or username == "ADMIN") and password == "12345") else print("Access denied")
+    
+    pass
+              
+def hows_the_weather(temperature):
+    if temperature < 40:
+        print("It's brisk out there!")
+    elif temperature >= 40 and temperature <= 65:
+        print("It's a little chilly out there!")
+    elif temperature > 85:
+        print("It's too dang hot out there!")
+    else:
+        print("It's perfect out there!")                  
+
     pass
 
-def hows_the_weather(temperature):
-    # your code here
-    pass
 
 def fizzbuzz(num):
-    # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif (num % 3 == 0):
+        print("Fizz")
+    elif(num % 5 == 0):
+        print("Buzz")
+    else:
+        print(num)            
+
     pass
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
+
+
+admin_login("sudo", "12345")
+
+admin_login("admin", "12345")
+
+admin_login("ADMIN", "12345")
+
+
+
+hows_the_weather(33)
+
+hows_the_weather(99)
+
+hows_the_weather(75)
+
+
+fizzbuzz(1)
+
+fizzbuzz(2)
+
+fizzbuzz(3)
+
+fizzbuzz(4)
+
+fizzbuzz(5)
+
+fizzbuzz(15)
+
+print(calculator("+", 1, 1))
+print(calculator("-", 3, 1))
+print(calculator("*", 3, 2))
+print(calculator("/", 4, 2))
+print(calculator("nope", 4, 2))
+
+
