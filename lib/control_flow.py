b69ab25113787022a979695cca072ea909cf52f9
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-08-19 00:24:57 +0300

    complete assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,34 +1,28 @@
-#!/usr/bin/env python3
-
 def admin_login(username, password):
-    print("Access granted") if ((username == "admin" or username == "ADMIN") and password == "12345") else print("Access denied")
-    
-    pass
-              
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
 def hows_the_weather(temperature):
     if temperature < 40:
-        print("It's brisk out there!")
+        return "It's brisk out there!"
     elif temperature >= 40 and temperature <= 65:
-        print("It's a little chilly out there!")
+        return "It's a little chilly out there!"
     elif temperature > 85:
-        print("It's too dang hot out there!")
+        return "It's too dang hot out there!"
     else:
-        print("It's perfect out there!")                  
-
-    pass
-
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
     if num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
-    elif (num % 3 == 0):
-        print("Fizz")
-    elif(num % 5 == 0):
-        print("Buzz")
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
     else:
-        print(num)            
-
-    pass
+        return num
 
 def calculator(operation, num1, num2):
     if operation == "+":
@@ -42,39 +36,26 @@ def calculator(operation, num1, num2):
     else:
         print("Invalid operation!")
         return None
+        
 
+# Test cases
+print(admin_login("sudo", "12345"))
+print(admin_login("admin", "12345"))
+print(admin_login("ADMIN", "12345"))
 
-admin_login("sudo", "12345")
+print(hows_the_weather(33))
+print(hows_the_weather(99))
+print(hows_the_weather(75))
 
-admin_login("admin", "12345")
-
-admin_login("ADMIN", "12345")
-
-
-
-hows_the_weather(33)
-
-hows_the_weather(99)
-
-hows_the_weather(75)
-
-
-fizzbuzz(1)
-
-fizzbuzz(2)
-
-fizzbuzz(3)
-
-fizzbuzz(4)
-
-fizzbuzz(5)
-
-fizzbuzz(15)
+print(fizzbuzz(1))
+print(fizzbuzz(2))
+print(fizzbuzz(3))
+print(fizzbuzz(4))
+print(fizzbuzz(5))
+print(fizzbuzz(15))
 
 print(calculator("+", 1, 1))
 print(calculator("-", 3, 1))
 print(calculator("*", 3, 2))
 print(calculator("/", 4, 2))
 print(calculator("nope", 4, 2))
-
-
